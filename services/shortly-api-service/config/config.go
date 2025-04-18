c6058425c7ce28af3d884b2c4c55a47f89714450
Rehan <rehanalire52@gmail.com>
2025-04-18 20:16:41 +0530

    fix: Refactor Init function to return error and use GetEnvOrPanic for environment variables

--- services/shortly-api-service/config/config.go
+++ services/shortly-api-service/config/config.go
@@ -1,14 +1,14 @@
 package config
 
 import (
-	"log"
+	"fmt"
 	"os"
 
 	"github.com/joho/godotenv"
 )
 
 type Config struct {
-	Port        string
+	PORT        string
 	DB_PORT     string
 	DB_HOST     string
 	DB_USER     string
@@ -19,22 +19,34 @@ type Config struct {
 
 var AppConfig Config
 
-func Init() {
+func Init() error {
 
 	err := godotenv.Load()
 
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		return err
 	}
 
 	AppConfig = Config{
-		Port:        os.Getenv("PORT"),
-		DB_PORT:     os.Getenv("DB_PORT"),
-		DB_HOST:     os.Getenv("DB_HOST"),
-		DB_USER:     os.Getenv("DB_USER"),
-		DB_NAME:     os.Getenv("DB_NAME"),
-		DB_PASSWORD: os.Getenv("DB_PASSWORD"),
-		JWT_SECRET:  os.Getenv("JWT_SECRET"),
+		PORT:        GetEnvOrPanic("PORT"),
+		DB_PORT:     GetEnvOrPanic("DB_PORT"),
+		DB_HOST:     GetEnvOrPanic("DB_HOST"),
+		DB_USER:     GetEnvOrPanic("DB_USER"),
+		DB_NAME:     GetEnvOrPanic("DB_NAME"),
+		DB_PASSWORD: GetEnvOrPanic("DB_PASSWORD"),
+		JWT_SECRET:  GetEnvOrPanic("JWT_SECRET"),
 	}
 
+	return nil
+}
+
+func GetEnvOrPanic(key string) string {
+
+	value := os.Getenv(key)
+
+	if value == "" {
+		panic(fmt.Sprintf("❌ Missing required environment variable: %s", key))
+	}
+
+	return value
 }
