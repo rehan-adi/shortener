16c86d2dabd5f1c37e9a88b33235ad5f6df6d8af
Rehan <rehanalire52@gmail.com>
2025-04-20 14:35:59 +0530

    feat: Implement initial URL handler functions for CRUD operations

---
+++ services/shortly-api-service/internal/handlers/url.go
@@ -0,0 +1,23 @@
+package handlers
+
+import "github.com/gin-gonic/gin"
+
+func CreateUrl(ctx *gin.Context) {
+
+}
+
+func GetAllUrls(ctx *gin.Context) {
+
+}
+
+func GetUrlDetails(ctx *gin.Context) {
+
+}
+
+func UpdateUrl(ctx *gin.Context) {
+
+}
+
+func DeleteUrl(ctx *gin.Context) {
+
+}
