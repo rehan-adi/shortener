3bbf0a569d8d71cbfe17590ca9dc695d37869655
Rehan <rehanalire52@gmail.com>
2025-04-20 15:23:08 +0530

    feat: Add initial configuration files for MongoDB and Redis environment variables

---
+++ services/shortly-kgs-service/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	MONGO_URI string
+}
+
+var AppConfig Config
+
+func Init() error {
+
+	err := godotenv.Load()
+
+	if err != nil {
+		return err
+	}
+
+	AppConfig = Config{
+		MONGO_URI: GetEnvOrPanic("MONGO_URI"),
+	}
+
+	return nil
+}
+
+func GetEnvOrPanic(key string) string {
+
+	value := os.Getenv(key)
+
+	if value == "" {
+		panic(fmt.Sprintf("❌ Missing required environment variable: %s", key))
+	}
+
+	return value
+
+}
