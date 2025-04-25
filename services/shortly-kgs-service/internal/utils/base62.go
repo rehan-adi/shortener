4930417cf301e4e59ef5922becf87135426d0293
Rehan <rehanalire52@gmail.com>
2025-04-25 22:12:57 +0530

    refactor: Replace Base62Encode function with GenerateRandomKey for random key generation

--- services/shortly-kgs-service/internal/utils/base62.go
+++ services/shortly-kgs-service/internal/utils/base62.go
@@ -1,25 +1,36 @@
 package utils
 
+import (
+	"crypto/rand"
+	"math/big"
+	"strings"
+)
+
 const charset = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
-func Base62Encode(n int64) string {
-	if n == 0 {
-		return padLeft(string(charset[0]), 6)
+func GenerateRandomKey(length int) (string, error) {
+
+	var result strings.Builder
+
+	base := int64(len(charset)) // 62 characters (Base62)
+
+	// Generate a random number in a large range
+	randNum, err := rand.Int(rand.Reader, big.NewInt(base))
+
+	if err != nil {
+		return "", err
 	}
 
-	result := ""
-	for n > 0 {
-		remainder := n % 62
-		result = string(charset[remainder]) + result
-		n /= 62
+	for randNum.Cmp(big.NewInt(0)) == 1 {
+		remainder := randNum.Int64() % base
+		result.WriteByte(charset[remainder])
+		randNum.Div(randNum, big.NewInt(base))
 	}
 
-	return padLeft(result, 6)
-}
-
-func padLeft(s string, length int) string {
-	for len(s) < length {
-		s = string(charset[0]) + s
+	key := result.String()
+	for len(key) < length {
+		key = string(charset[0]) + key
 	}
-	return s
+
+	return key, nil
 }
