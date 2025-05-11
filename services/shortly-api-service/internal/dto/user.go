cade4e28eec1e649d75270e053a0d86615eb846c
Rehan <rehanalire52@gmail.com>
2025-05-11 22:46:01 +0530

    fix: Remove UrlsCount field from UserDTO structure

--- services/shortly-api-service/internal/dto/user.go
+++ services/shortly-api-service/internal/dto/user.go
@@ -6,7 +6,6 @@ type UserDTO struct {
 	ID        uint      `json:"id"`
 	Email     string    `json:"email"`
 	Username  string    `json:"username"`
-	UrlsCount int       `json:"url_count"`
 	CreatedAt time.Time `json:"created"`
 }
 
