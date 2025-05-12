d66f44e06b64015b9d811b137dc7826caf121d9e
Rehan <rehanalire52@gmail.com>
2025-05-12 18:30:12 +0530

    fix: Add rate limiting to URL routes for improved request handling

--- services/shortly-api-service/internal/routes/url.go
+++ services/shortly-api-service/internal/routes/url.go
@@ -9,25 +9,26 @@ import (
 
 func UrlRouter(router *gin.RouterGroup) {
 
+	// Redirect to Original Url
+	router.GET("/url/redirect/:shortKey", middlewares.RateLimiter("50-m"), handlers.RedirectToOriginalUrl)
+
 	url := router.Group("/url").Use(middlewares.AuthMiddleware())
 
 	{
 		// Get all URLs (for login user)
-		url.GET("/", handlers.GetAllUrls)
+		url.GET("/", middlewares.RateLimiter("20-M"), handlers.GetAllUrls)
 
 		// Shorten a URL
-		url.POST("/shorten", handlers.CreateUrl)
+		url.POST("/shorten", middlewares.RateLimiter("5-M"), handlers.CreateUrl)
 
 		// Get URL details by shortKey
-		url.GET("/:shortKey", handlers.GetUrlDetails)
+		url.GET("/:shortKey", middlewares.RateLimiter("20-M"), handlers.GetUrlDetails)
 
 		// Update an existing URL
-		url.PATCH("/:shortKey", handlers.UpdateUrl)
+		url.PATCH("/:shortKey", middlewares.RateLimiter("5-M"), handlers.UpdateUrl)
 
 		// Delete a URL
-		url.DELETE("/:shortKey", handlers.DeleteUrl)
+		url.DELETE("/:shortKey", middlewares.RateLimiter("5-M"), handlers.DeleteUrl)
 	}
-
-	url.GET("/redirect/:shortKey", handlers.RedirectToOriginalUrl)
 
 }
