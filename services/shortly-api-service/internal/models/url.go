c47dfd30b7d1c3c6f553d1578b604a74a183855a
Rehan <rehanalire52@gmail.com>
2025-05-12 18:32:28 +0530

    refactor: Remove unused ExpiresAt field from Url model

--- services/shortly-api-service/internal/models/url.go
+++ services/shortly-api-service/internal/models/url.go
@@ -1,20 +1,17 @@
 package models
 
 import (
-	"time"
-
 	"gorm.io/gorm"
 )
 
 type Url struct {
 	gorm.Model
 
-	OriginalURL string  `gorm:"not null"`
-	ShortKey    string  `gorm:"size:50;uniqueIndex;not null"`
-	Title       string  `gorm:"size:255"`
-	UserID      *string `gorm:"index"`
-	User        *User   `gorm:"foreignKey:UserID"`
-	Clicks      int     `gorm:"default:0"`
-	ExpiresAt   *time.Time
+	OriginalURL string      `gorm:"not null"`
+	ShortKey    string      `gorm:"size:50;uniqueIndex;not null"`
+	Title       string      `gorm:"size:255"`
+	UserID      *string     `gorm:"index"`
+	User        *User       `gorm:"foreignKey:UserID"`
+	Clicks      int         `gorm:"default:0"`
 	Analytics   []Analytics `gorm:"foreignKey:UrlID"`
 }
